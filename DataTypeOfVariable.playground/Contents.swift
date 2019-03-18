d225992c1257ad780cdbee45ecc4fe75968a56a9
nam <sudjai.sa59@snru.ac.th>
2019-03-18 14:28:21 +0700

    Start DattaType

---
+++ DataTypeOfVariable.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+//String Type ตัวแปรที่เป็นอักษร
+
+var myName = "Doramoon" //นี่คือการประกาศที่อาศัยชนิดของ Value บอก  Datatype
+var mySurname:String = "AAAAAA" //นี่คือการประกาศแบบ กำหนด Datatype ตั้งแต่เกิดพ
+
+
+
+
---
+++ VarialbleAndContant.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//Explicait Variable
+//การประกาศตัวแปร นะจ้ะ
+
+var myName = "Nam"
+myName = "Doramon"
+
+//Explicait Connstant
+let myFrind = "Nopita"
+//myFrind = "Sunako" Error เพราะ myFrind เป็นค่าคงที่  let
+
