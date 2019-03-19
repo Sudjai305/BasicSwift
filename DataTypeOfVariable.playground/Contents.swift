071514c8cd42f3d07bf86b9470e4410878c14876
nam <sudjai.sa59@snru.ac.th>
2019-03-19 15:55:06 +0700

    Class and Fumction

--- DataTypeOfVariable.playground/Contents.swift
+++ DataTypeOfVariable.playground/Contents.swift
@@ -3,8 +3,37 @@ import UIKit
 //String Type ตัวแปรที่เป็นอักษร
 
 var myName = "Doramoon" //นี่คือการประกาศที่อาศัยชนิดของ Value บอก  Datatype
-var mySurname:String = "AAAAAA" //นี่คือการประกาศแบบ กำหนด Datatype ตั้งแต่เกิดพ
+var mySurname:String = "AAAAAA" //นี่คือการประกาศแบบ กำหนด Datatype ตั้งแต่เกิด
+
+//การแสดงผลบน Console
+print("This is Console")
+print("myName =\(myName)")
 
 
+//Integer Type หรือ จำนวนที่ไม่มีเศษ
+var number1 = 100
+var number2:Int = 200
+
+
+
+//Double Type
+var number3 = 3.1416
+var number4: Double = 10.234
+
+//Boolean Type คือ จำนวนที่มีแค่ค่า True, false
+var status = true
+var status2: Bool = false
+
+//Array Type
+var myFriends = ["Doramon","Nopita","Sunako","Chisuka"]
+print("myFriends ที่มี index = 1 มีค่าเท่ากับ => \(myFriends[1])")
+
+//การเพิ่มสมาชิคให้กับ Array
+myFriends.append("Nam")
+print(myFriends)
+
+//การลบสมาชิค ของ Array
+myFriends.remove(at: 1)
+print(myFriends)
 
 
